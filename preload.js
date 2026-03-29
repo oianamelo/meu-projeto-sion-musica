439aafddadac72c61b5268135c50c659e8e23edc
oianamelo <oi.anamelo@gmail.com>
2026-03-29 12:46:03 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expor API segura para o renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  callAPI: (apiKey, prompt) => ipcRenderer.invoke('api-call', { apiKey, prompt })
-});
