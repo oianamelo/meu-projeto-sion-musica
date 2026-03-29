360450c498a50d170e779eac2a3e57875e5bef08
oianamelo <oi.anamelo@gmail.com>
2026-03-29 12:19:10 -0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,13 @@
 export default async function handler(req, res) {
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -17,8 +25,6 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-
-    res.setHeader("Access-Control-Allow-Origin", "*");
 
     res.status(200).json(data);
 
